9cbdd11beb9ede855edbe23d26bac35aa9581cf9
Payal Nayee <PNayee@spartaglobal.com>
2019-08-19 10:12:30 +0100

    python cookbook updated

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+apt_update 'update_sources' do
+  action :update
+end
+
+package 'python3' do
+  action :install
+end
+
+package 'python-pip' do
+  action :install
+end
+
+execute 'python-pip install plugins' do
+  command 'pip install flask==0.10.1'
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -17,13 +17,15 @@ describe 'python::default' do
     end
   end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'default apt update' do
+      expect { chef_run }.to update_apt_update 'update_sources'
     end
+
+    it "should install python" do
+      expect { chef_run }.to install_package "python"
+    end
+
+    it "should install python pip" do
+      expect { chef_run }.to install_package "python_pip"
   end
 end
